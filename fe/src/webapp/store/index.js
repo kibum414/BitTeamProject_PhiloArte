3f4285a845e18e511a2f229bf195246ec9ced979
kibum414 <kibum414@naver.com>
2021-05-12 20:23:38 +0900

    Front

--- fe/src/webapp/store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
-import users from '../user/reducer/user.reducer'
-
-const rootReducer = combineReducers({ users })
-export default configureStore({
-  reducer: rootReducer,
-  middleware: [...getDefaultMiddleware(), logger]
-})
